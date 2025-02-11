e9c68828c6d94b292a2f643f9252a4ac09bda0ab
PesukarhuTG <ganzhinovat@yandex.ru>
2025-02-11 23:33:04 +0300

    feat: implemet logic of AddTask component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import AddTask from './components/AddTask';
 import TaskItem from './components/TaskItem';
 
+type Task = {
+  id: string;
+  text: string;
+  status: 'inProgress' | 'completed';
+};
+
 const App = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  // Добавление новой задачи
+  const addTask = (taskText: string) => {
+    const newTask: Task = {
+      id: Math.random().toString(16).substring(2, 10),
+      text: taskText,
+      status: 'inProgress',
+    };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div className="app-container vh-100 w-100 d-flex align-items-center justify-content-center flex-column">
       <h3>Todo App</h3>
-      <AddTask />
+      <AddTask onSave={addTask} />
       <div className="table-wrapper">
         <table className="table table-hover table-bordered">
           <thead>
--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,15 +1,38 @@
-const AddTask = () => {
+import { FormEvent, useState } from 'react';
+
+interface Props {
+  onSave: (task: string) => void;
+}
+
+const AddTask = ({ onSave }: Props) => {
+  const [taskText, setTaskText] = useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (taskText.trim()) {
+      onSave(taskText);
+      setTaskText(''); // Очищаем поле после добавления задачи
+    }
+  };
+
   return (
-    <form className="d-flex align-items-center mb-3">
+    <form className="d-flex align-items-center mb-3" onSubmit={handleSubmit}>
       <label className="form-group me-3 mb-0">
-        <input type="text" className="form-control" placeholder="ввести задачу" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Ввести задачу"
+          value={taskText}
+          onChange={(e) => setTaskText(e.target.value)}
+          aria-label="Ввод задачи"
+        />
       </label>
 
-      <button type="submit" className="btn btn-primary me-3">
+      <button type="submit" className="btn btn-primary me-3" disabled={!taskText.trim()}>
         Сохранить
       </button>
 
-      <button type="reset" className="btn btn-warning">
+      <button type="reset" className="btn btn-warning" onClick={() => setTaskText('')}>
         Очистить
       </button>
     </form>
